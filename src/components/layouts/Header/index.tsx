84ceef7386e7b2a031afab8dd8ccf68e04297e54
fitronansori <fitronansori7@gmail.com>
2024-09-04 17:06:28 +0800

    feat: Add scroll functionality to Header component

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -5,12 +5,35 @@ import { Link } from "react-router-dom";
 import ProfileDropdown from "@/components/ProfileDropdown";
 import CartDropdown from "@/components/CartDropdown";
 import MobileMenu from "./MobileMenu";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
   const login = false;
 
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scroll]);
+
   return (
-    <header className="shadow-sm">
+    <header
+      className={cn(
+        "w-full shadow-sm",
+        scroll
+          ? "sticky top-0 left-0 bg-white shadow-md transition-all duration-300 z-10"
+          : "block bg-transparent"
+      )}>
       <div className="container h-14 sm:h-16 flex items-center justify-between">
         <div className="flex items-center gap-x-10">
           <Logo />
