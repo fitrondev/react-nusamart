b9a86279aab158342260f2d744fcbc0ed0ceb329
fitronansori <fitronansori7@gmail.com>
2024-09-05 11:04:59 +0800

    feat: Add new components for homepage layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Categories from "./components/layouts/Categories";
+import Features from "./components/layouts/Features";
 import Hero from "./components/layouts/Hero";
+import ProductSection from "./components/layouts/ProductSection";
 
 const App = () => {
   return (
     <>
       <Hero />
+      <Features />
+      <Categories />
+      <ProductSection />
     </>
   );
 };
