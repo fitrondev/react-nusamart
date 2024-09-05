ca5d066994b6f5010755a27e7faea5d47d6d0bc7
fitronansori <fitronansori7@gmail.com>
2024-09-05 11:04:07 +0800

    feat: Add Categories component

---
+++ src/components/layouts/Categories/index.tsx
@@ -0,0 +1,33 @@
+import { categories } from "@/assets/images/data";
+import TitleSection from "@/components/TitleSection";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <section className="my-10 py-10">
+      <div className="container">
+        <TitleSection title="Select Categories" />
+
+        <div className="mt-10 grid grid-cols-5 place-items-center gap-5">
+          {categories.map((item, index) => (
+            <Link
+              to={`/products/${item.name}`}
+              key={index}
+              className="hover:scale-110 transition-all duration-300 ease-in-out">
+              <div className="space-y-2">
+                <div className="size-28 bg-rose-300 flex items-center justify-center rounded-lg">
+                  <img src={item.image} alt={item.name} className="size-full" />
+                </div>
+
+                <div className="text-center text-lg font-medium">
+                  <h3>{item.name}</h3>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Categories;
